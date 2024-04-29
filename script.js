3450a836d836575847433cc7ae4cf8810612e2fa
jvjv2020 <162923498+jvjv2020@users.noreply.github.com>
2024-04-29 02:11:22 +0200

    New UI with different buttons - all working New UI without with memory and color buttons JS changes in function inputOutput and calculate changes css colored 1-3 for color changes

--- script.js
+++ script.js
@@ -3,13 +3,15 @@
 const numberField = document.querySelector('#numberField');
 const oppDisplay = document.querySelector('#opp');
 const resultDisplay = document.querySelector('#result');
+const container = document.querySelector('.container');
+let memory = 0;
+let indexColor = 0;
 
 numberField.addEventListener(('click'), event => {
     if (event.target.tagName === 'BUTTON') {
-        const buttonText = event.target.dataset.value;
-        return processInput(buttonText);
-    }
-
+        const buttonValue = event.target.dataset.value;
+        return processInput(buttonValue);
+    };
 });
 
 
@@ -19,39 +21,148 @@ function processInput(buttonValue) {
         outputInput(buttonValue);
         return
     }
-     calculate();
-
-
+    calculate();
 }
 
-
+// processes the input on shows numbers and operators on the UI display 
 function outputInput(buttonValue) {
 
-    if (buttonValue.includes("$")) {
-        buttonValue = buttonValue.slice(1);
+    let oppDisplayText = oppDisplay.textContent;
+
+    // delete old result if more input
+    if (oppDisplayText !== '=' && buttonValue !== 'M+' && buttonValue !== 'M-')
+        resultDisplay.textContent = '=';
+
+    // if input isNaN
+    if (isNaN(buttonValue)) {
         switch (buttonValue) {
             case "del":
                 oppDisplay.textContent = '';
+                resultDisplay.textContent = '=';
                 break;
             case "back":
-                let oppDisplayText = oppDisplay.textContent;
-                oppDisplayText = oppDisplayText.slice(0, -1);
-                oppDisplay.textContent = oppDisplayText;
+                if (oppDisplayText.slice(-1) === ' ') {
+                    oppDisplay.textContent = oppDisplayText.slice(0, -3);
+                    break;
+                }
+                oppDisplay.textContent = oppDisplayText.slice(0, -1);
+                resultDisplay.textContent = '=';
+                break;
+            case "Mdel":
+                memory = 0;
+                break;
+            case "M":
+                oppDisplay.textContent += memory;
+                break;
+            case "M+":
+                if (resultDisplay.textContent !== "=")
+                    memory += memory + Number(resultDisplay.textContent);
+                break;
+            case "M-":
+                if (resultDisplay.textContent !== "=")
+                    memory += memory - Number(resultDisplay.textContent);
+                break;
+            case "-":
+                if (oppDisplayText === '' || oppDisplayText.slice(-1) === ' ') {
+                    oppDisplay.textContent += '-'
+                    break;
+                }
+                oppDisplay.textContent += ' - ';
+                break;
+            case "color":
+                let remove = indexColor;
+                indexColor++;
+                if (indexColor === 3)
+                    indexColor = 0;
+                container.classList.add(`colored${indexColor}`);
+                container.classList.remove(`colored${remove}`);
+                break;
+            case ",":
+                if (oppDisplayText === '' || oppDisplayText.slice(-1) === ' ')
+                    break;
+                oppDisplay.textContent += ',';
                 break;
             default:
-                oppDisplay.textContent += ` ${buttonValue} `;
+                // check if first input an operation, then break
+                if (oppDisplayText === '')
+                    break;
+                // check if last char a ' ', then last input was operation and should be overwritten
+                if (oppDisplayText.slice(-1) === ' ') {
+                    oppDisplay.textContent = `${oppDisplayText.slice(0, -3)} ${buttonValue} `;
+                } else
+                    oppDisplay.textContent += ` ${buttonValue} `;
         }
-
-    }else{
+        // if input is a number
+    } else {
         oppDisplay.textContent += buttonValue;
     }
 }
 
 
-function calculate(){
-    const oppDisplayText = oppDisplay.textContent;
-    let result = eval(oppDisplayText);
+function calculate() {
+    const oppDisplayText = oppDisplay.textContent.replace(/,/g, '.');
+    // let result = eval(oppDisplayText);
+
+    calculation = oppDisplayText.split(' ');
+    let interimCalc;
 
 
-    resultDisplay.textContent = result; 
+    // calculate X^y and y root of X
+    while (true) {
+
+        let foundIndex = calculation.findIndex(item => item === '**' || item === 'rt');
+        if (foundIndex === -1) break;
+
+        switch (calculation[foundIndex]) {
+            case ('**'):
+                interimCalc = calculation[foundIndex - 1] ** calculation[foundIndex + 1];
+                break;
+            case ('rt'):
+                interimCalc = calculation[foundIndex - 1] ** (1 / calculation[foundIndex + 1]);
+
+        }
+        calculation.splice(foundIndex - 1, 3, interimCalc)
+
+    }
+
+
+    // calculate *,/
+    while (true) {
+
+        let foundIndex = calculation.findIndex(item => item === '*' || item === '/');
+        if (foundIndex === -1) break;
+
+        switch (calculation[foundIndex]) {
+            case ('*'):
+                interimCalc = calculation[foundIndex - 1] * calculation[foundIndex + 1];
+                break;
+            case ('/'):
+                interimCalc = calculation[foundIndex - 1] / calculation[foundIndex + 1];
+
+        }
+        calculation.splice(foundIndex - 1, 3, interimCalc)
+
+    }
+
+    // calculate +,-
+    while (true) {
+
+        let foundIndex = calculation.findIndex(item => item === '+' || item === '-');
+        if (foundIndex === -1) break;
+
+        switch (calculation[foundIndex]) {
+            case ('+'):
+                interimCalc = parseFloat(calculation[foundIndex - 1]) + parseFloat(calculation[foundIndex + 1]);
+                break;
+            case ('-'):
+                interimCalc = calculation[foundIndex - 1] - calculation[foundIndex + 1];
+
+        }
+        calculation.splice(foundIndex - 1, 3, interimCalc)
+
+    }
+
+
+
+    resultDisplay.textContent = calculation[0];
 }
